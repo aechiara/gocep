a4c5021705ee6462557f0afce377a068ce81bba5
aechiara <aechiara@gmail.com>
2018-06-26 09:46:34 -0300

    regex working

--- gocep.go
+++ gocep.go
@@ -6,6 +6,7 @@ import "net/http"
 import "net/url"
 import "io/ioutil"
 import "strings"
+import "regexp"
 
 type SitemapIndex struct {
 	Locations [] string `xml:"sitemap>loc"`
@@ -38,7 +39,20 @@ func main() {
 	//fmt.Println("response Status:", resp.Status)
 	//fmt.Println("response Headers:", resp.Header)
 
-	body, _ := ioutil.ReadAll(resp.Body)
+	b, _ := ioutil.ReadAll(resp.Body)
+	body := string(b)
 
-	fmt.Println(string(body))
+	//fmt.Println(body)
+
+	re := regexp.MustCompile("(?s)(?m)<table class=\"tmptabela\">(.*?)</table>")
+	output := re.FindString(body)
+	fmt.Println("--------")
+	fmt.Printf("[%q]\n", output)
+	fmt.Println("--------")
+
+	reg, err := regexp.Compile("[^a-zA-Z0-9<>= /]+")
+	processedString := reg.ReplaceAllString(output, "")
+	fmt.Printf("[%q]\n", processedString)
+
+
 }
